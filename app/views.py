7261da8f382139f594e7a40515cd764265869ef0
MirkoSDev <mirkostadelman@outlook.com>
2024-11-16 15:59:45 -0300

    consigna minima

--- app/views.py
+++ app/views.py
@@ -11,8 +11,9 @@ def index_page(request):
 # esta función obtiene 2 listados que corresponden a las imágenes de la API y los favoritos del usuario, y los usa para dibujar el correspondiente template.
 # si el opcional de favoritos no está desarrollado, devuelve un listado vacío.
 def home(request):
-    images = []
-    favourite_list = []
+    search_input = request.GET.get('search', None)  # Obtiene el parámetro 'search' de la URL, si existe.
+    images = services.getAllImages(search_input)
+    favourite_list = services.getAllFavourites(request)
 
     return render(request, 'home.html', { 'images': images, 'favourite_list': favourite_list })
 
